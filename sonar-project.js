8337b6f098df4e8fbf97fa19e94397c746e55288
sammybrandy <sammybrandy2001@yahoo.com>
2023-01-17 06:00:45 +0000

    sonar-nexus conf

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.183.125.250:9000/',
+  serverUrl: 'http://172.31.1.223:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'nonejs',
-	    'sonar.login': '887e4525dcce3964e26a88a36aad934cdcf3e1f6',
+	    'sonar.login': 'b1706e5b87a07ed6ec568c6f11ac99a5ac492d13',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
